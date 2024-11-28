d80f913466913372992c8806965b855eee8a2a9c
Artur Aronov <unionco@icloud.com>
2024-11-28 14:40:59 +0800

    iz3: add echo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,26 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
 
+    public static void main(String[] args) throws Exception {
         while(true) {
             System.out.print("$ ");
-            Scanner scanner = new Scanner(System.in);
-            String input = scanner.nextLine();
-            if(input.equals("exit 0")) {
-                System.exit(0);
+            Input input = new Input();
+            String userInput = input.getInput();
+
+            switch(userInput) {
+                case "exit 0" -> System.exit(0);
+                case String s when s.startsWith("echo") -> {
+                    InputParser inputParser = new InputParser(s, "echo");
+                    String statement = inputParser.getStatement();
+                    if(statement == null) {
+                        System.out.println(input.getInput() + ": command not found");
+                    } else {
+                        System.out.println(inputParser.getStatement());
+                    }
+                }
+                default -> System.out.println(input.getInput() + ": command not found");
             }
-            System.out.println(input + ": command not found");
         }
     }
 }
