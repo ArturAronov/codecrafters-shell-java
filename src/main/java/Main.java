16ce35a5a63db711e7687629f172878b92fafe88
Artur Aronov <unionco@icloud.com>
2024-11-28 10:49:15 +0800

    pn5: add exit 0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,9 @@ public class Main {
             System.out.print("$ ");
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
+            if(input.equals("exit 0")) {
+                System.exit(0);
+            }
             System.out.println(input + ": command not found");
         }
     }
