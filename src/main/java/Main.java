a4971b34e136b1a90660a6ba3b1cd6a2421efe95
Artur Aronov <unionco@icloud.com>
2024-12-03 10:51:34 +0800

    Main: rm print

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         String[] commands = {"echo", "exit", "type"};
-        System.out.println(args);
         String[] path = System.getenv("PATH").split(":");
 //        for(String p: path) {
 //            System.out.println(p);
