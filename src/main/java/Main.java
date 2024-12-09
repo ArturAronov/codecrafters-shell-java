f4a68cc2125984926de87148885fc36b928af605
Artur Aronov <unionco@icloud.com>
2024-12-09 10:42:32 +0800

    ip1: minor fixes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,6 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         String[] commands = {"echo", "exit", "type"};
--- src/main/java/PathWalker.java
+++ src/main/java/PathWalker.java
@@ -1,8 +1,6 @@
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Files;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 
 public class PathWalker {
     private final String[] paths;
@@ -21,29 +19,5 @@ public class PathWalker {
         }
 
         return null;
-    }
-
-    public String runExecutable(String command) {
-        try {
-            String executablePath = getAvailablePath(command);
-            if(executablePath == null) {
-                return null;
-            }
-
-            Process process = new ProcessBuilder(executablePath).start();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            System.out.println(reader.readLine());
-            StringBuilder output = new StringBuilder();
-            String line;
-
-            while((line = reader.readLine()) != null) {
-                output.append(line);
-            }
-
-            process.waitFor();
-            return output.toString().trim();
-        } catch(Exception e) {
-            return null;
-        }
     }
 }
